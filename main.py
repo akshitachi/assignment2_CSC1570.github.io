ebcb17618eb54de267b2a3ce1ba4e56279d0bc2b
Akshil <shahakshil2001@gmail.com>
2024-02-14 16:40:50 -0800

    get data news

--- main.py
+++ main.py
@@ -13,27 +13,32 @@ def getData():
     current_date = current_datetime.date()
     from_datetime = current_datetime - relativedelta(months=6, days=1)
     from_date = from_datetime.date()
+    from_datetime2 = current_datetime-relativedelta(days=30)
+    from_date2 = from_datetime2.date()
     symbol = request.args.get("searchText")
     endpoint = f' https://finnhub.io/api/v1/stock/profile2?symbol={symbol}&token={api_key}'
     endpoint2= f' https://finnhub.io/api/v1/quote?symbol={symbol}&token={api_key}'
     endpoint3 = f'https://finnhub.io/api/v1/stock/recommendation?symbol={symbol}&token={api_key}'
     endpoint4 = f'https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/{from_date}/{current_date}?adjusted=true&sort=asc&apiKey={polygon_api_key}'
+    endpoint5 = f'https://finnhub.io/api/v1/company-news?symbol={symbol}&from={from_date2}&to={current_date}&token={api_key}'
     response = requests.get(endpoint)
     response2 = requests.get(endpoint2)
     response3 = requests.get(endpoint3)
     response4 = requests.get(endpoint4)
+    response5 = requests.get(endpoint5)
     if response.status_code==200 and response2.status_code==200:
+        data5=response5.json()
         data4=response4.json()
         data3 = response3.json()
         data2=response2.json()
         data = response.json()
+        print(data5)
         all_data = {
         'data1': data,
         'data2': data2,
         'data3': data3,
         'data4': data4,
         'date':datetime.today().strftime('%Y-%m-%d'),
-        # Add more data as needed
     }
         return jsonify(all_data)
     else:
